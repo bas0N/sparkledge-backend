8f183a49295ad88dc14e7e167411864c2f7d8f35
basON <wch.basinski@gmail.com>
2022-03-14 17:34:45 +0100

    create JWT tokens

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,6 +6,10 @@ const usersDB = {
 };
 const bcrypt = require("bcrypt");
 const { response } = require("express");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const fspromises = require("fs").promises;
+const path = require("path");
 
 const handleLogin = async (req, res) => {
   const { email, password } = req.body;
@@ -18,7 +22,33 @@ const handleLogin = async (req, res) => {
   if (!foundUser) return res.sendStatus(401); //Unauthorised
   const comparison = bcrypt.compare(password, foundUser.password);
   if (comparison) {
-    res.json({ success: `User ${email} is logged in` });
+    const accessToken = jwt.sign(
+      {
+        email: foundUser.email,
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "45s" }
+    );
+    const refreshToken = jwt.sign(
+      {
+        email: foundUser.email,
+      },
+      process.env.REFRESH_TOKEN_SECRET,
+      { expiresIn: "1d" }
+    );
+    //take care of it
+    otherUsers = usersDB.users.filter(
+      (person) => person.email != foundUser.email
+    );
+    currentUser = { ...foundUser, refreshToken };
+    usersDB.setUsers([...otherUsers, currentUser]);
+    //write the user to the file
+    console.log(currentUser);
+    res.cookie("jwt", refreshToken, {
+      httpOnly: true,
+      maxAge: 24 * 60 * 60 * 1000,
+    }); //one day valid cookie
+    res.json({ accessToken });
   } else {
     res.sendStatus(401); //Unautorised
   }
