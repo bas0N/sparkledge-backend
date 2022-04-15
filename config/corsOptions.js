9a9ae62803409066c48bfc78846e86f7c28d7099
basON <wch.basinski@gmail.com>
2022-04-15 18:10:44 +0200

    cors edit

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -3,6 +3,22 @@ const allowedOrigins = require("./allowedOrigins");
 //cors
 const corsOptions = {
   origin: (origin, callback) => {
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("CORS ERROR"));
+    }
+  },
+  optionsSuccessStatus: 200,
+};
+
+module.exports = corsOptions;
+
+/*const allowedOrigins = require("./allowedOrigins");
+
+//cors
+const corsOptions = {
+  origin: (origin, callback) => {
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
@@ -13,3 +29,4 @@ const corsOptions = {
 };
 
 module.exports = corsOptions;
+*/
